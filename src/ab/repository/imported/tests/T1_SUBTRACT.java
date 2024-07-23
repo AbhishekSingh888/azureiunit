dd7922ba74954296d7ec66807b2c526016a2a5e3
Abhishek Singh <absingh@arcadsoftware.com>
2024-07-23 13:08:01 +0530

    commit

---
+++ src/ab/repository/imported/tests/T1_SUBTRACT.java
@@ -0,0 +1,33 @@
+package ab.repository.imported.tests;
+
+import java.util.Collection;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.skyscreamer.jsonassert.JSONAssert;
+import com.arcadsoftware.iunit.junit.xml.IUnitXMLParser;
+import com.arcadsoftware.iunit.junit.model.Configuration;
+import com.arcadsoftware.iunit.junit.model.ResourceManager;
+
+@RunWith(value = Parameterized.class)
+public class T1_SUBTRACT {
+
+	static String inputXMLFilePath = "testcases/ab.repository.imported.tests/T1_SUBTRACT.xml";
+	String input;
+	String expectedResult;
+	String compareJson;
+	boolean isFailureAssertion = false;
+	boolean isErrorAssertion = false;
+
+	public T1_SUBTRACT(String input, String expectedResult, String compareJson) {
+		this.input = input;
+		this.expectedResult = expectedResult;
+		this.compareJson = compareJson;
+	}
+
+	@Parameters
+	public static Collection<String[]> testData() {
+		return IUnitXMLParser.getParameters("resources/ab.repository.imported.tests/T1_SUBTRACT");
+	}
+}
---
+++ src/ab/repository/imported/tests/TEST1_MULTIPLY.java
@@ -0,0 +1,33 @@
+package ab.repository.imported.tests;
+
+import java.util.Collection;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.skyscreamer.jsonassert.JSONAssert;
+import com.arcadsoftware.iunit.junit.xml.IUnitXMLParser;
+import com.arcadsoftware.iunit.junit.model.Configuration;
+import com.arcadsoftware.iunit.junit.model.ResourceManager;
+
+@RunWith(value = Parameterized.class)
+public class TEST1_MULTIPLY {
+
+	static String inputXMLFilePath = "testcases/ab.repository.imported.tests/TEST1_MULTIPLY.xml";
+	String input;
+	String expectedResult;
+	String compareJson;
+	boolean isFailureAssertion = false;
+	boolean isErrorAssertion = false;
+
+	public TEST1_MULTIPLY(String input, String expectedResult, String compareJson) {
+		this.input = input;
+		this.expectedResult = expectedResult;
+		this.compareJson = compareJson;
+	}
+
+	@Parameters
+	public static Collection<String[]> testData() {
+		return IUnitXMLParser.getParameters("resources/ab.repository.imported.tests/TEST1_MULTIPLY");
+	}
+}
